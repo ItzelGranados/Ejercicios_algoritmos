4ca8ab7d7f76ef313dfcdb4fa5696f77c3c257fa
ItzelGranados <itzelgranados13@gmail.com>
2022-05-25 16:46:24 -0500

    Primer commit de los ejercicios Javascript

---
+++ js/main.js
@@ -0,0 +1,68 @@
+//Seleccionar el número mayor entre 3 números
+/*let a = prompt("Ingresa un número entre 1 y 100:");
+let b = prompt("Ingresa un número entre 1 y 100:");
+let c = prompt("Ingresa un número entre 1 y 100:");
+
+
+if ((a >= b) && (a >= c)) {
+    alert("El número mayor es:" + a);
+} else if ((b >= a) && (b >= c)) {
+    alert("El número mayor es:" + b);
+} else if ((c >= a) && (c >= b)) {
+    alert("El número mayor es:" + c);
+}
+
+
+//Seleccionar el número menor entre 3 números
+let d = prompt("Ingresa un número entre 1 y 100:");
+let e = prompt("Ingresa un número entre 1 y 100:");
+let f = prompt("Ingresa un número entre 1 y 100:");
+
+if ((d <= e) && (d <= f)) {
+    alert("El número menor es:" + d);
+} else if ((e <= d) && (e <= f)) {
+    alert("El número menor es:" + e);
+} else if ((f <= d) && (f <= e)) {
+    alert("El menor número es:" + f);
+}
+
+
+//Determinar si un número es múltiplo de 3
+let entero = prompt("Ingresa un número entre 1 y 200:");
+
+if (entero % 3 == 0) {
+    alert("Múltiplo");
+} else {
+    alert("No es múltiplo u.u");
+}*/
+
+//Leer 3 números y determinar si uno es la suma de los otros dos
+
+/*let num1 = prompt("Ingresa un número:");
+let num2 = prompt("Ingresa un número:");
+let num3 = prompt("Ingresa un número:");
+
+if ((num1 + num2) == num3) {
+    console.log("Son la suma del número 3");
+} else if ((num1 + num3) == num2) {
+    console.log("Son la suma del número 2");
+} else if ((num3 + num2) == num1) {
+    console.log("Son la suma del número 1");
+} else {
+    console.log("Ninguno es suma");
+}*/
+
+
+
+//Determinar si un número es par o impar
+/*let par = prompt("Ingresa un número:");
+
+if (par % 2 == 0) {
+    alert("Es par");
+} else {
+    alert("Es impar");
+}*/
+
+function add {
+
+}
